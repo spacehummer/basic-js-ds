8f73cc3cd71ac80679e73968ecc8b60bc75f5fb7
Yuriy Lavrushin <yuriy.lavroushin@gmail.com>
2023-04-17 21:14:04 +0400

    feat: solution for `Remove from list` is ready.

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,13 +1,18 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
+
+
 
 /**
  * Given a singly linked list of integers l and an integer k,
  * remove all elements from list l that have a value equal to k.
  *
- * @param {List} l
- * @param {Number} k
+ * @param {{value: *, next: Number} | *} l  - linked list
+ *                                            (from constructor '../extensions/list-node.js').
+ *                                            or next value (in recursive call).
+ * @param {Number}      k
+ // * @param {Number}      next  - next value for analysis (used in recursive call).
  * @return {List}
  *
  * @example
@@ -22,10 +27,35 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  // Recursive function!!!
+
+  if (l === null) {
+    return null;
+  }
+
+  // throw new NotImplementedError('Not implemented');
+  // // remove line with error and write your code here
+
+  console.log("---- Source data:", l,',', k);
+
+  if (l.value === k) {
+    return l.next ? removeKFromList(l.next, k) : null;
+  } else {
+    l.next = removeKFromList(l.next, k);
+  }
+
+  console.log("---- New list:", l);
+
+  return l;
+
 }
+
+// Test scripts
+// npm run test > "./logs/log_all_tests_-_$(date +%Y-%m-%d_-_%k_%M_%S).logs"
+// npm run test ./test/remove-from-list.test.js  > "./logs/log_remove_from_list_-_$(date +%Y-%m-%d_-_%k_%M_%S).logs"
+
+// removeKFromList();
 
 module.exports = {
   removeKFromList
